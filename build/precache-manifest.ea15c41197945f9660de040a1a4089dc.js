b0f9d219fbd61ec8f13f58677dce9ec986551882
soulDa <strikingda@gmail.com>
2020-01-01 20:43:05 +0800

    服务器太垃圾，还是把build带上

---
+++ build/precache-manifest.ea15c41197945f9660de040a1a4089dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544b52ec24a842941636d0a94a197808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8b53050d993e2f65ffe",
+    "url": "/static/css/2.f8716875.chunk.css"
+  },
+  {
+    "revision": "7f18b97a0e0b3538f346",
+    "url": "/static/css/main.6b5203f4.chunk.css"
+  },
+  {
+    "revision": "c8b53050d993e2f65ffe",
+    "url": "/static/js/2.69c09aff.chunk.js"
+  },
+  {
+    "revision": "843625b6150579175745aa2caaaccf95",
+    "url": "/static/js/2.69c09aff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f18b97a0e0b3538f346",
+    "url": "/static/js/main.d5c6fe76.chunk.js"
+  },
+  {
+    "revision": "787c95033c3a84400538",
+    "url": "/static/js/runtime-main.55a406f6.js"
+  }
+]);
